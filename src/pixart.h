2007ca96617a32d223ceaf411845e9b609bfd35e
Daytona0306 <y.o.19910306@gmail.com>
2025-12-07 23:34:42 +0900

    Update pixart.h

--- src/pixart.h
+++ src/pixart.h
@@ -10,6 +10,7 @@
 #include <zephyr/drivers/spi.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/sensor.h>
+#include <zephyr/kernel.h> // ★重要: k_sem, k_threadを使うために必要
 
 #ifdef __cplusplus
 extern "C" {
@@ -28,6 +29,15 @@ struct pixart_data {
 
     bool                         ready; // whether init is finished successfully
     int                          err; // error code during async init
+
+    // ★★★ ここから追加 (VIPスレッド用) ★★★
+    // これがないと pmw3610.c のコンパイルが通りません
+    struct k_sem                 gpio_sem;
+    struct k_thread              thread;
+    
+    // スレッド用のスタックメモリ確保 (1024バイト)
+    K_KERNEL_STACK_MEMBER(thread_stack, 1024);
+    // ★★★ 追加ここまで ★★★
 };
 
 // device config data structure
